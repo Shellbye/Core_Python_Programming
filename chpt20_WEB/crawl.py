018183f2d8f592fb408a555e67c83603c736a13a
shellbye <shellbye.com@gmail.com>
2014-08-26 16:00:21 +0800

    crawl.py 中 filename 注释

--- chpt20_WEB/crawl.py
+++ chpt20_WEB/crawl.py
@@ -1,57 +1,66 @@
 #!/usr/bin/env python
-
-from sys import argv
+# -*- coding:utf-8 -*-
 from os import makedirs, unlink, sep
-from os.path import isdir, exists, dirname, splitext
+from os.path import isdir, exists, dirname, splitext, abspath
 from string import replace, find, lower
 from htmllib import HTMLParser
 import urllib
-from urllib import urlretrieve
 from urlparse import urlparse, urljoin
 from formatter import DumbWriter, AbstractFormatter
 from cStringIO import StringIO
 
-class Retriever(object):	# download Web pages
+
+class Retriever(object):    # download Web pages
 
     def __init__(self, url):
         self.url = url
         self.file = self.filename(url)
 
     def filename(self, url, deffile='index.htm'):
-        parsedurl = urlparse(url, 'http:', 0)  # parse path
+        # 使用http协议解析url地址，将url解析为这样的五元组：(scheme, netloc, path, query, fragment)
+        parsedurl = urlparse(url, 'http:', 0)
+        # 将主机地址（netloc）和路径（path）合并起来作为存储文件的路径名
         path = parsedurl[1] + parsedurl[2]
+        # splitext将path分割为路径与后缀名，如（/path/to/file, txt）
         ext = splitext(path)
+        # 如果后缀名为空，则给当前path添加默认的名字index.htm
         if ext[1] == '':
+            # 若path结尾有“/”则直接添加index.htm，否则先添加“/”
             if path[-1] == '/':
                 path += deffile
             else:
                 path += '/' + deffile
-        ldir = dirname(path)	# local directory
-        if sep != '/':		# os-indep. path separator
-            ldir = replace(ldir, ',', sep)
+        # 取出path中的目录部分（www.shellbye.com\\blog），然后与本地目录结合为一个目录('D:\\www.shellbye.com\\blog')
+        ldir = dirname(abspath(path))
+        # 如果不是以“/”作为目录分割符的类Unix系统，比如Windows，则需要把“”替换为相应的目录分割符
+        # 因为类Unix系统的目录分割符与URI地址的分割符一样，所以可以不处理
+        if sep != '/':        # os-indep. path separator
+            ldir = replace(ldir, '/', sep)
+        # 如果ldir目录不存在在创建
         if not isdir(ldir):      # create archive dir if nec.
-            if exists(ldir): unlink(ldir)
+            # 如果ldir存在但是不是目录则删除ldir。注，unlink即remove。
+            if exists(ldir):
+                unlink(ldir)
             makedirs(ldir)
         return path
 
-    def download(self):		# download Web page
+    def download(self):        # download Web page
         try:
             retval = urllib.urlretrieve(self.url, self.file)
         except IOError:
-            retval = ('*** ERROR: invalid URL "%s"' % \
-                self.url, )
+            retval = ('*** ERROR: invalid URL "%s"' % self.url, )
         return retval
 
-    def parseAndGetLinks(self):	# pars HTML, save links
-        self.parser = HTMLParser(AbstractFormatter( \
-            DumbWriter(StringIO())))
+    def parseAndGetLinks(self):    # pars HTML, save links
+        self.parser = HTMLParser(AbstractFormatter(DumbWriter(StringIO())))
         self.parser.feed(open(self.file).read())
         self.parser.close()
-        return self.parse.anchorlist
+        return self.parser.anchorlist
 
-class Crawler(object):		# manage entire crawling process
 
-    count = 0			# static downloaded page counter
+class Crawler(object):        # manage entire crawling process
+
+    count = 0            # static downloaded page counter
 
     def __init__(self, url):
         self.q = [url]
@@ -64,7 +73,7 @@ class Crawler(object):		# manage entire crawling process
         if retval[0] == '*':     # error situation, do not parse
             print retval, '... skipping parse'
             return
-        Crawler.count = Crawler.count + 1
+        Crawler.count += 1
         print '\n(', Crawler.count, ')'
         print 'URL:', url
         print 'FILE:', retval[0]
@@ -72,8 +81,7 @@ class Crawler(object):		# manage entire crawling process
 
         links = r.parseAndGetLinks()  # get and process links
         for eachLink in links:
-            if eachLink[:4] != 'http' and \
-                    find(eachLink, '://') == -1:
+            if eachLink[:4] != 'http' and find(eachLink, '://') == -1:
                 eachLink = urljoin(url, eachLink)
             print '* ', eachLink,
 
@@ -91,25 +99,19 @@ class Crawler(object):		# manage entire crawling process
                     else:
                         print '... discarded, already in Q'
             else:
-                    print '... discarded, already processed'
+                print '... discarded, already processed'
 
     def go(self):                # process links in queue
         while self.q:
             url = self.q.pop()
             self.getPage(url)
 
-def main():
-    if len(argv) > 1:
-        url = argv[1]
-    else:
-        try:
-            url = raw_input('Enter starting URL: ')
-        except (KeyboardInterrupt, EOFError):
-            url = ''
 
-    if not url: return
+def main():
+    url = "http://shellbye.com"
     robot = Crawler(url)
     robot.go()
+
 
 if __name__ == '__main__':
     main()
